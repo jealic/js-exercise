4be8d049fb204c478854f766ce974a5be44d4b64
Ling Chiehyun <jealic@gmail.com>
2018-08-08 22:05:04 +0800

    todo list function

--- js/todo.js
+++ js/todo.js
@@ -1,2 +1,37 @@
 'user strict';
 
+function createToDo(){
+  var todo = document.createElement("div");
+  var span = document.createElement("span");
+  var replaceButton = document.createElement("button");
+  var removeButton = document.createElement("button");
+
+  var input = document.getElementById("input").value;
+  if (input == "") {
+    input = "廢文一篇";
+  }
+  span.innerHTML = input;
+  todo.appendChild(span);
+
+  replaceButton.onclick = function() {
+    var input = document.getElementById("input").value;
+    if (input == "") {
+      alert("你並沒有輸入任何文字")
+      return;
+    }
+    this.parentNode.lastChild.innerHTML = input;
+    document.getElementById("input").value = "";
+  }
+  replaceButton.textContent = "R";
+  todo.appendChild(replaceButton);
+
+  removeButton.textContent = "V";
+  todo.appendChild(removeButton);
+  removeButton.onclick = function() {
+    this.parentNode.parentNode.removeChild(this.parentNode);
+  }
+  
+  document.getElementById("todolist").appendChild(todo);
+  document.getElementById("input").value = "";
+
+}
